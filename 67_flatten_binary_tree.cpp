26f9c8c174662b37956847dfc229ce2a84e9caac
aayushii9602 <83585995+aayushii9602@users.noreply.github.com>
2023-04-27 23:33:09 +0530

    flatten a binary tree into a linked list

---
+++ 67_flatten_binary_tree.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+    node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void flat(node *root)
+{
+    if (root == NULL || (root->left == NULL && root->right == NULL))
+    {
+        return;
+    }
+    if (root->left != NULL)
+    {
+        flat(root->left);
+        node *temp = root->right;
+        root->right = root->left;
+        root->left = NULL;
+        node *t = root->right;
+        while (t->right != NULL)
+            t = t->right;
+        t->right = temp;
+    }
+}
+
+void inorder(node *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main()
+{
+    struct node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+    flat(root);
+    inorder(root);
+
+    return 0;
+}
